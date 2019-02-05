827aa7a7d7f5b26c98b85cad1a5ed1dc866618f8
Petar Najman <petar.najman@modoolar.com>
2019-02-05 14:58:18 +0100

    [MIG] web_diagram_position: Migration to version 12.0

--- web_diagram_position/__manifest__.py
+++ web_diagram_position/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2017 - 2018 Modoolar <info@modoolar.com>
+# Copyright 2019 Modoolar <info@modoolar.com>
 # License LGPLv3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
 
 {
@@ -6,14 +6,12 @@
     "summary": "This module enables user to persist positions of elements in"
                "Diagram view",
     "category": "Web",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "LGPL-3",
     "author": "Modoolar",
     "website": "https://www.modoolar.com/",
     "images": ["static/description/banner.png"],
-    "depends": [
-        "web"
-    ],
+    "depends": ["web"],
     "data": [
         "views/web_diagram_position.xml",
     ],
